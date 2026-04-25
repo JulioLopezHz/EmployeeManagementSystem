a99cea06bdfa6a7a7db18ad1924be2e131e7511d
Julio López <prosis.jlopez@gmail.com>
2026-04-24 18:11:53 -0600

    Register page, adapted. First endpoint created

---
+++ EmployeeManagementSystem/Controllers/StatesController.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagementSystem.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class StatesController : ControllerBase
+{
+    private readonly IStatesService _StatesService;
+
+    public StatesController(IStatesService StatesService)
+    {
+        _StatesService = StatesService;
+    }
+
+    [HttpGet("GetAllStates")]
+    public async Task<ActionResult> GetAllStates()
+    {
+        var result = await _StatesService.GetStatesAsync();
+        return result != null ? Ok(result) : StatusCode(500);
+    }
+
+    [HttpGet("GetState")]
+    public async Task<ActionResult> GetState(int IdState)
+    {
+        var result = await _StatesService.GetStateAsync(IdState);
+        return result != null ? Ok(result) : StatusCode(500);
+    }
+}
